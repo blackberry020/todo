b868636c295d71efc2af88b1a29d9c1302ccc728
blackberry020 <ilonafrolova00@gmail.com>
2023-08-10 13:04:49 +0300

    error scroll

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
-      child: Builder(
-        builder: (context) {
-          return MaterialApp(
-            title: 'Todo app',
-            theme: ThemeData(
+      child: Builder(builder: (context) {
+        return MaterialApp(
+          title: 'Todo app',
+          theme: ThemeData(
               useMaterial3: true,
-              colorScheme: context.watch<MyAppState>().isDarkTheme ? const ColorScheme.dark() : ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 18, 211, 44))
-            ),
-            home: MyHomePage(),
-            debugShowCheckedModeBanner: false,
-          );
-        }
-      ),
+              colorScheme: context.watch<MyAppState>().isDarkTheme
+                  ? const ColorScheme.dark()
+                  : ColorScheme.fromSeed(
+                      seedColor: const Color.fromARGB(255, 18, 211, 44))),
+          home: MyHomePage(),
+          debugShowCheckedModeBanner: false,
+        );
+      }),
     );
   }
 }
@@ -36,7 +36,6 @@ class MyAppState extends ChangeNotifier {
   bool isDarkTheme = false;
 
   void addNewTodo() {
-
     var todoToAdd = newTodoController.text.trim();
 
     if (todoToAdd != "") {
@@ -55,100 +54,93 @@ class MyAppState extends ChangeNotifier {
 }
 
 class Todo extends StatelessWidget {
+  final String todo;
 
-  late String todo;
+  const Todo({required this.todo});
 
-  Todo(String _todo) {
-    todo = _todo;
-  }
+  void moveToDone() {}
+
+  void deleteTodo() {}
 
   @override
   Widget build(BuildContext context) {
-
-    void moveToDone() {
-      
-    }
-
-    void deleteTodo() {
-      
-    }
-
-    return Row(
-        children: [
-          ListTile(
-            leading: const Icon(Icons.today),
-            title: Text(todo),
-          ),
-          IconButton(
-            onPressed: moveToDone, 
-            icon: Icon(Icons.done),
-          ),
-          IconButton(
-            onPressed: deleteTodo, 
-            icon: Icon(Icons.delete),
-          ),
-        ],
-      );
+    return ListTile(
+        leading: const Icon(Icons.today),
+        title: Text(todo),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              onPressed: moveToDone,
+              icon: const Icon(Icons.done),
+            ),
+            IconButton(
+              onPressed: deleteTodo,
+              icon: const Icon(Icons.delete),
+            ),
+          ],
+        ));
   }
 }
 
 class TodoPage extends StatelessWidget {
-  @override 
+  @override
   Widget build(BuildContext context) {
-
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 200,
-                  child: TextFormField(
-                    controller: appState.newTodoController,
-                    decoration: const InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  hintText: 'Write your new TODO',
-                                ),
-                    onFieldSubmitted: (String? todoToAdd) {
-                      appState.addNewTodo();
-                    },
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 200,
+                child: TextFormField(
+                  controller: appState.newTodoController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Write your new TODO',
                   ),
+                  onFieldSubmitted: (String? todoToAdd) {
+                    appState.addNewTodo();
+                  },
                 ),
-                const SizedBox(
-                  width: 10,
-                ),
-                ElevatedButton(
-                  onPressed: appState.addNewTodo, 
-                  child: const Text('Add'),
-                )
-              ],
-            ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              ElevatedButton(
+                onPressed: appState.addNewTodo,
+                child: const Text('Add'),
+              )
+            ],
           ),
-          SizedBox(
-            height: 300,
-            child: ListView(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Text('You have '
-                      '${appState.todos.length} TODOs:', textAlign: TextAlign.center,),
+        ),
+        SizedBox(
+          height: 300,
+          child: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  'You have '
+                  '${appState.todos.length} TODOs:',
+                  textAlign: TextAlign.center,
                 ),
-                if (appState.todos.isNotEmpty)
-                  for (var cur in appState.todos)
-                    ListTile(
-                      leading: const Icon(Icons.today),
-                      title: Text(cur),
-                    ),
-              ],
-            ),
+              ),
+              if (appState.todos.isNotEmpty)
+                ListView.builder(
+                    itemCount: appState.todos.length,
+                    itemBuilder: (context, index) => SizedBox(
+                          height: 500,
+                          child: Todo(todo: appState.todos.elementAt(index)),
+                        )),
+            ],
           ),
-        ]
-      ),
+        ),
+      ]),
     );
   }
 }
@@ -156,31 +148,31 @@ class TodoPage extends StatelessWidget {
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
-      body: 
-        SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Text(
-                  'Dark theme',
-                   style: TextStyle(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.w500),
-                ),
-                const SizedBox(width: 10),
-                Switch(
-                 value: appState.isDarkTheme,
-                 onChanged: (val) {
-                   appState.themeChanged(val);
-                 },
-                ),
-              ],
-            ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Text(
+                'Dark theme',
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(width: 10),
+              Switch(
+                value: appState.isDarkTheme,
+                onChanged: (val) {
+                  appState.themeChanged(val);
+                },
+              ),
+            ],
           ),
         ),
+      ),
     );
   }
 }
@@ -191,7 +183,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   final pages = [TodoPage(), const Placeholder(), SettingsPage()];
 
   @override
@@ -199,46 +190,38 @@ class _MyHomePageState extends State<MyHomePage> {
     var appState = context.watch<MyAppState>();
 
     return SafeArea(
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          return Scaffold(
-            body: Row(
-              children: [
-                  NavigationRail(
-                    extended: constraints.maxWidth >= 500,
-                    destinations: const [
-                      NavigationRailDestination(
-                        icon: Icon(Icons.today), 
-                        label: Text('TODO')
-                      ),
-                      NavigationRailDestination(
-                        icon: Icon(Icons.done), 
-                        label: Text('Done')
-                      ),
-                      NavigationRailDestination(
-                        icon: Icon(Icons.settings), 
-                        label: Text('Settings')
-                      ),
-                    ], 
-                    selectedIndex: appState.selectedPageIndex,
-                    onDestinationSelected: (int newIndex) {
-                      setState(() {
-                        appState.selectedPageIndex = newIndex;
-                        if (newIndex >= pages.length) throw "there is no page for $newIndex";
-                      });
-                    },
-                    backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-                  ),
-                Expanded(
-                  child: Container(
-                            child: pages[appState.selectedPageIndex],
-                          )
+      child: LayoutBuilder(builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              NavigationRail(
+                extended: constraints.maxWidth >= 500,
+                destinations: const [
+                  NavigationRailDestination(
+                      icon: Icon(Icons.today), label: Text('TODO')),
+                  NavigationRailDestination(
+                      icon: Icon(Icons.done), label: Text('Done')),
+                  NavigationRailDestination(
+                      icon: Icon(Icons.settings), label: Text('Settings')),
+                ],
+                selectedIndex: appState.selectedPageIndex,
+                onDestinationSelected: (int newIndex) {
+                  setState(() {
+                    appState.selectedPageIndex = newIndex;
+                    if (newIndex >= pages.length)
+                      throw "there is no page for $newIndex";
+                  });
+                },
+                backgroundColor: Theme.of(context).colorScheme.primaryContainer,
               ),
-              ],
-            ),
-          );
-        }
-      ),
+              Expanded(
+                  child: Container(
+                child: pages[appState.selectedPageIndex],
+              )),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
